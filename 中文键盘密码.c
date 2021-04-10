a60ec62829a77ffc6eee4fc0cb1d3babb841a3f8
hxrlz <81855262+hxrlz@users.noreply.github.com>
2021-04-10 18:08:11 +0800

    Add files via upload

---
+++ 中文键盘密码.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+main()
+{
+	char a;
+	scanf("%c",&a);
+	switch(a)
+	{
+		case 'a':printf("%s","s"); break;
+		case 'b':printf("%s","n"); break;
+		case 'c':printf("%s","v"); break;
+		case 'd':printf("%s","f"); break;
+		case 'e':printf("%s","r"); break;
+		case 'f':printf("%s","g"); break;
+		case 'g':printf("%s","h"); break;
+		case 'h':printf("%s","j"); break;
+		case 'i':printf("%s","o"); break;
+		case 'j':printf("%s","k"); break;
+		case 'k':printf("%s","l"); break;
+		case 'l':printf("%s","a"); break;
+		case 'm':printf("%s","z"); break;
+		case 'n':printf("%s","m"); break;
+		case 'o':printf("%s","p"); break;
+		case 'p':printf("%s","q"); break;
+		case 'q':printf("%s","w"); break;
+		case 'r':printf("%s","t"); break;
+		case 's':printf("%s","d"); break;
+		case 't':printf("%s","y"); break;
+		case 'u':printf("%s","i"); break;
+		case 'v':printf("%s","b"); break;
+		case 'w':printf("%s","e"); break;
+		case 'x':printf("%s","c"); break;
+		case 'y':printf("%s","u"); break;
+		case 'z':printf("%s","x"); break;
+	}
+ } 
